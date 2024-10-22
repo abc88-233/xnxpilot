749ac244d16a14f7053b5d1a0da563f7b1715768
abc88-233 <52513595+abc88-233@users.noreply.github.com>
2024-10-22 10:00:46 +0800

    asd

---
+++ selfdrive/VgetK/sotpasyncreqrep.cc
@@ -0,0 +1,9 @@
+#include"sotpasyncreqrep.hpp"
+
+using namespace etas::getk;
+class Impl1:public etas::getk::api::ISoTpAsyncRequest{
+    ErrorCodeT find(const types::ServiceStrListT& srvStrList)
+    {
+        
+    }
+}
