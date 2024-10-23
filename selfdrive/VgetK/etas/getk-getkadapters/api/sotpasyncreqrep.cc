7731dd874c3058e262349c08a820d703f0186bc3
abc88-233 <52513595+abc88-233@users.noreply.github.com>
2024-10-23 09:58:10 +0800

    123

--- selfdrive/VgetK/etas/getk-getkadapters/api/sotpasyncreqrep.cc
+++ selfdrive/VgetK/etas/getk-getkadapters/api/sotpasyncreqrep.cc
@@ -5,53 +5,49 @@ class Impl1:public etas::getk::api::ISoTpAsyncRequest{
     ErrorCodeT find(const types::ServiceStrListT& srvStrList)
     {
        
-      //寻找所有标识为serviceShortIds的服务
-        types::ServiceDescListT& offer_list;
-        types::ServiceDescListT& stop_list;
-
-        for(const auto& srv : srvStrList){
-           //寻找以string标识的通道是否为活跃状态
-           auto it = serv.find(srv);
-           if(it != serv.end())
-           {
-            SubSocket* subsock = it->second;
-            if(subsock && subsock->alive)
-            { //如果为活跃状态，则offer_list添加
-                offer_list.push_back();
-            }
-            else{
-                 //如果为不活跃状态，则stop_list添加
-               stop_list.push_back();
-           }
-           }
-          else{
-
-          }
-   
-          return EC
-           
-        }
-
-        
-        //返回给Ralo状态信息
         ISoTpAsyncResponse* responseHandler;
-        if(!responseHandler)
-        {
-            return EC_ERR_INVALID_HANDLE;
+         if (!responseHandler) {
+            return EC_ERR_INVALID_HANDLE; // 返回错误代码，因为响应处理器未设置
         }
-        //如果是活跃状态，则offer
-        
-        responseHandler->offer()
-        //如果是不活跃状态，则stopoffer
-        responseHandler ->stopOffer();
+        if(!offer_list.empty())
+        {
+            responseHandler->offer(offer_list);
+        }
+        if (!stop_list.empty()) {
+            responseHandler->stopOffer(stop_list);
+        }
+        return EC_OK;
         
     }
+
+
     ErrorCodeT findAll(){
         //寻找所有活跃的通道
-             
+        types::ServiceDescListT& offer_list;
+        types::ServiceDescListT& stop_list;
+        for (const auto& it : services) {
+          SubMaster sm({(it.first).c_str()});
+          if(sm.allAlive)
+          {
+            offer_list.push_back(types::ServiceDescT(it.second.port,it.first))
+          }
+          else{
+            stop_list.push_back(types::ServiceDescT(it.second.port,it.first))
+          }
+        }
 
         //返回给Ralo状态信息
-        ISoTpAsyncResponse ->offer();
-        ISoTpAsyncResponse ->stopOffer();
+        ISoTpAsyncResponse* responseHandler;
+         if (!responseHandler) {
+            return EC_ERR_INVALID_HANDLE; // 返回错误代码，因为响应处理器未设置
+        }
+        if(!offer_list.empty())
+        {
+            responseHandler->offer(offer_list);
+        }
+        if (!stop_list.empty()) {
+            responseHandler->stopOffer(stop_list);
+        }
+        return EC_OK;
     }
 }
