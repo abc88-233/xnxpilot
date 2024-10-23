65d1c0296570d2f1a5f52e1a8132dce195fc4739
abc88-233 <52513595+abc88-233@users.noreply.github.com>
2024-10-23 10:26:18 +0800

    123

--- selfdrive/VgetK/etas/getk-getkadapters/api/sotpasyncreqrep.cc
+++ selfdrive/VgetK/etas/getk-getkadapters/api/sotpasyncreqrep.cc
@@ -2,35 +2,43 @@
 #include
 using namespace etas::getk;
 class Impl1:public etas::getk::api::ISoTpAsyncRequest{
-    ErrorCodeT find(const types::ServiceStrListT& srvStrList)
+    ErrorCodeT find(const types::ServiceStrListT& srvStrList) override
     {
+        //设置两个ServiceDescListT类型列表，用于向VgetK系统传递活跃与不活跃的通道消息
         types::ServiceDescListT& offer_list;
         types::ServiceDescListT& stop_list;
+        //遍历需要查找的服务列表
         for(const auto& srvStr: srvStrList){
+            //如果列表不在我们的服务中，则说明参数传递有误
             if(services.find(srvStr)!=services.end())
             {
                 return EC_ERR_INVALID_PARAMETER;
             }
+            //查找该通道是否活跃
             SubMaster sm({srvStr.c_str()});
             auto mes = services[ss];
+            //活跃则放入offer_list中
             if(sm.allAlive)
             {
                 offer_list.push_back(types::ServiceDescT(mes.port,ss));
             }
+              //不活跃则放入stop_list中
             else{
                 stop_list.push_back(types::ServiceDescT(mes.port,ss));
             }
         }
-       
+       //定义ISoTpAsyncResponse类用于发送响应
         ISoTpAsyncResponse* responseHandler;
          if (!responseHandler) {
             return EC_ERR_INVALID_HANDLE; // 返回错误代码，因为响应处理器未设置
         }
         if(!offer_list.empty())
         {
+            //发送活跃通道
             responseHandler->offer(offer_list);
         }
         if (!stop_list.empty()) {
+            //发送不活跃通道
             responseHandler->stopOffer(stop_list);
         }
         return EC_OK;
@@ -38,7 +46,8 @@ class Impl1:public etas::getk::api::ISoTpAsyncRequest{
     }
 
 
-    ErrorCodeT findAll(){
+    ErrorCodeT findAll() override
+    {
         //寻找所有活跃的通道
         types::ServiceDescListT& offer_list;
         types::ServiceDescListT& stop_list;
@@ -69,3 +78,17 @@ class Impl1:public etas::getk::api::ISoTpAsyncRequest{
     }
 }
 
+ErrorCodeT subscribe(const types::MappedServiceListT& mpdSrvList) override
+{
+    //服务订阅设置回调流程
+}
+
+ErrorCodeT unsubscribe(const types::MappedServiceListT& mpdSrvList) override
+{
+
+}
+
+ErrorCodeT unsubscribeAll() override
+{
+
+}
